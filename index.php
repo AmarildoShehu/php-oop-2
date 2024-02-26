8b22db758d20559ee9717f794d1daf6791ad173d
Amarildo <you@example.com>
2024-02-27 00:41:30 +0100

    php and added style to html

--- index.php
+++ index.php
@@ -13,12 +13,14 @@ class Product {
     }
 
     public function displayCard() {
-        echo "<div class='card'>
-                <img src='{$this->image}' alt='{$this->title}' />
-                <h2>{$this->title}</h2>
-                <p>Prezzo: {$this->price} €</p>
-                <p>Categoria: {$this->category}</p>
-                <p>Tipo: Prodotto Generico</p>
+        $categoryDetails = $this->category->displayCard();
+        return "<div class='card'>
+                <img src='{$this->image}' alt='{$this->title}' class='card-img-top' />
+                <div class='card-body'>
+                    <h2 class='card-title'>{$this->title}</h2>
+                    <p class='card-text'>Prezzo: {$this->price} €</p>
+                    <p class='card-text'>Categoria: {$categoryDetails}</p>
+                </div>
             </div>";
     }
 }
@@ -37,10 +39,7 @@ class Category {
     }
 
     public function displayCard() {
-        echo "<div class='category-card'>
-                <h2>{$this->name}</h2>
-                <p>{$this->privateValue}</p>
-            </div>";
+        return "<h2>{$this->name}</h2>";
     }
 }
 
@@ -54,22 +53,19 @@ class FoodProduct extends Product {
 
     public function displayCard() {
         $foodDetails = "<p>Tipo: Cibo - {$this->foodType}</p>";
-        $categoryDetails = $this->category->displayCard();
-        echo parent::displayCard() . $foodDetails . $categoryDetails;
+        return parent::displayCard() . $foodDetails;
     }
 }
 
 class ToyProduct extends Product {
     public function displayCard() {
-        $categoryDetails = $this->category->displayCard();
-        echo parent::displayCard() . "<p>Tipo: Gioco</p>" . $categoryDetails;
+        return parent::displayCard() . "<p>Tipo: Gioco</p>";
     }
 }
 
 class AccessoryProduct extends Product {
     public function displayCard() {
-        $categoryDetails = $this->category->displayCard();
-        echo parent::displayCard() . "<p>Tipo: Accessorio</p>" . $categoryDetails;
+        return parent::displayCard() . "<p>Tipo: Accessorio</p>";
     }
 }
 
@@ -84,17 +80,36 @@ $royalCanin = new FoodProduct("Royal Canin Mini Adult", 15.99, "https://arcaplan
 $almoNatureCatDaily = new FoodProduct("Almo Nature Cat Daily Lattina", 3.99, "https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg", $catCategory, "Lattina");
 $kongClassic = new ToyProduct("Kong Classic", 8.49, "https://arcaplanet.vtexassets.com/arquivos/ids/256599/kong-classic1.jpg", $dogCategory);
 $volieraWilma = new AccessoryProduct("Voliera Wilma in Legno", 79.99, "https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg", $birdCategory);
-
-// Visualizzazione delle card
-$dogCategory->displayCard();
-$royalCanin->displayCard();
-
-$catCategory->displayCard();
-$almoNatureCatDaily->displayCard();
-
-$dogCategory->displayCard();
-$kongClassic->displayCard();
-
-$birdCategory->displayCard();
-$volieraWilma->displayCard();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+<div class="container">
+        <h1 class="mt-5 mb-4 text-center">Benvenuto al Negozio di Animali</h1>
+
+        <div class="row">
+            <div class="col-md-3 product-card">
+                <?php echo $royalCanin->displayCard(); ?>
+            </div>
+            <div class="col-md-3 product-card">
+                <?php echo $almoNatureCatDaily->displayCard(); ?>
+            </div>
+            <div class="col-md-3 product-card">
+                <?php echo $kongClassic->displayCard(); ?>
+            </div>
+            <div class="col-md-3 product-card">
+                <?php echo $volieraWilma->displayCard(); ?>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
